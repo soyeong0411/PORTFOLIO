00b137d1479c4102b562ada6d5f5c5a7b6ef2729
soyeong0411 <2so092@naver.com>
2022-10-26 18:13:45 +0900

    1026

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+//menu
 $(".subMenuBg").hide();
 $(".menuWrap>ul>li").hover(function(){
     $(this).children(".submenu").stop().slideDown(400);
@@ -7,3 +7,43 @@ $(".menuWrap>ul>li").hover(function(){
 },function(){
     $(".subMenuBg,.menuWrap .submenu").stop().hide();
 })
+
+//swiper-slide
+var swiper = new Swiper(".mainSlide", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".noticeSlide", {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//slick-slide
+$('.slick-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite : true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots : true,
+    // prevArrow : "<button type='button' class='slick-prev'>Previous</button>",		// 이전 화살표 모양 설정
+	// nextArrow : "<button type='button' class='slick-next'>Next</button>",		// 다음 화살표 모양 설정
+  });
